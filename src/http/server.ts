e47702a6782a5a16617a0a4ba32420a9d4df8da7
nathancamolez-dev <nathan.camolez@unesp.br>
2024-09-25 15:06:28 -0300

    feat: created seeding file

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,9 @@
+import chalk from "chalk";
 import Elysia from "elysia";
 
 
 const app = new Elysia()
 
 app.listen(3333, () =>{
-  console.log("Server started on port 3333")
+  console.log(chalk.green("Server started on port 3333"))
 } )
